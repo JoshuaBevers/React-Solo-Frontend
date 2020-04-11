17515f1b06b3aa23ee766a57a6c5083f149101ca
infernezor@gmail.com <infernezor@gmail.com>
2020-04-11 00:27:42 -0400

    Added profile. Displays player's stats.

---
+++ ui/src/components/profile.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Player } from "../interfaces/player";
+
+class Profile extends Component {
+  state = {
+    player: {},
+  };
+
+  getPlayer = () => {
+    const LoadedPlayer = new Player(1, "Joshua", 1, 1, 1, 1, 1, 1, 10); // this can connect to backend to grab a user easily later. Given time available.
+    this.setState({ player: LoadedPlayer });
+  };
+
+  componentDidMount() {
+    this.getPlayer();
+  }
+
+  render() {
+    const {
+      name,
+      hp,
+      str,
+      int,
+      con,
+      idle,
+      relaxed,
+      wis,
+      level,
+    } = this.state.player;
+    return (
+      <div>
+        Name: {name}
+        <br />
+        <br />
+        level: {level}
+        <br />
+        Health: {hp}
+        <br />
+        Strength: {str}
+        <br />
+        Intelect: {int}
+        <br />
+        Constitution: {con}
+        <br />
+        Wisdom: {wis}
+        <br />
+        Idleness: {idle}
+        <br />
+        Relaxed: {relaxed}
+      </div>
+    );
+  }
+}
+
+export default Profile;
--- ui/src/interfaces/player.ts
+++ ui/src/interfaces/player.ts
@@ -4,11 +4,34 @@ export class Player {
   experience: number;
   level: number;
   hp: number;
-
-  constructor(id, name) {
+  str: number;
+  int: number;
+  wis: number;
+  con: number;
+  relaxed: number;
+  idle: number;
+  //strength, int, wis, con, relaxed, idle.
+  constructor(
+    id: number,
+    name: string,
+    hp: number,
+    str: number,
+    int: number,
+    wis: number,
+    con: number,
+    relaxed: number,
+    idle: number
+  ) {
     this.id = id;
     this.name = name;
     this.level = 1;
     this.experience = 0;
+    this.hp = hp;
+    this.str = str;
+    this.int = int;
+    this.wis = wis;
+    this.con = con;
+    this.relaxed = relaxed;
+    this.idle = idle;
   }
 }
